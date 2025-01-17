65b5adee12bd200a34d7c19a38da94295f72f3d9
zezotaha1 <zezo.taha12345@gmail.com>
2025-01-18 01:14:37 +0200

    SOME UPDATES

--- ExaminationSystem/Triggers.sql
+++ ExaminationSystem/Triggers.sql
@@ -1,3 +1,35 @@
+USE[ExaminationSystemDB]
+
+GO
+
+CREATE OR ALTER TRIGGER [Exam].TRG_Update_Exam_Degree
+ON [Exam].[Exam_Questions]
+AFTER INSERT, UPDATE, DELETE
+AS
+BEGIN
+    IF (UPDATE([Exam_Id]))
+    BEGIN
+        UPDATE e
+        SET e.Total_Exam_Degree = e.Total_Exam_Degree - d.Degree
+        FROM [Exam].[Exams] e
+        JOIN deleted d ON e.Id = d.Exam_Id;
+
+        UPDATE e
+        SET e.Total_Exam_Degree = e.Total_Exam_Degree + i.Degree
+        FROM [Exam].[Exams] e
+        JOIN inserted i ON e.Id = i.Exam_Id;
+    END
+
+    ELSE IF (UPDATE([Degree]))
+    BEGIN
+        UPDATE e
+        SET e.Total_Exam_Degree = e.Total_Exam_Degree + (i.Degree - d.Degree)
+        FROM [Exam].[Exams] e
+        JOIN inserted i ON e.Id = i.Exam_Id
+        JOIN deleted d ON i.Exam_Id = d.Exam_Id AND i.Id= d.Id; 
+    END
+END
+
 
 ----trigger to prevent add Question of Type not Trueand false
 
@@ -61,3 +93,70 @@ END CATCH
 END
 ----dddddddddd-
 
+CREATE OR ALTER TRIGGER [Answer].Clac_Student_Answer_Degree_TEXT
+ON[Answer].[Text_Answer]
+AFTER INSERT, UPDATE, DELETE
+AS 
+BEGIN 
+	
+	IF exists(select * from deleted)
+	BEGIN
+		 UPDATE [Answer].[Student_Answer]
+		SET [Answer_Degree] = 0
+		WHERE [Id] = (SELECT [Answer_Id] FROM deleted);
+	END
+	
+	DECLARE @Answer_Id INT = (SELECT [Answer_Id] FROM inserted);
+	
+	DECLARE @Degree FLOAT;
+	EXEC dbo.CalculateStudentAnswersResults @Student_Answer_Id = @Answer_Id,@Result =@Degree OUTPUT ;
+	UPDATE [Answer].[Student_Answer]
+	SET [Answer_Degree] = @Degree
+	WHERE [Id] = @Answer_Id;
+END
+
+
+CREATE OR ALTER TRIGGER [Answer].Clac_Student_Answer_Degree_MCQ
+ON[Answer].[Mcq_Answer]
+AFTER INSERT, UPDATE, DELETE
+AS 
+BEGIN 
+	
+	IF exists(select * from deleted)
+	BEGIN
+		 UPDATE [Answer].[Student_Answer]
+		SET [Answer_Degree] = 0
+		WHERE [Id] = (SELECT [Answer_Id] FROM deleted);
+	END
+	
+	DECLARE @Answer_Id INT = (SELECT [Answer_Id] FROM inserted);
+	
+	DECLARE @Degree FLOAT;
+	EXEC dbo.CalculateStudentAnswersResults @Student_Answer_Id = @Answer_Id,@Result =@Degree OUTPUT ;
+	UPDATE [Answer].[Student_Answer]
+	SET [Answer_Degree] = @Degree
+	WHERE [Id] = @Answer_Id;
+END
+
+
+CREATE OR ALTER TRIGGER [Answer].Clac_Student_Answer_Degree_True_False
+ON[Answer].[True_False_Answer]
+AFTER INSERT, UPDATE, DELETE
+AS 
+BEGIN 
+	
+	IF exists(select * from deleted)
+	BEGIN
+		 UPDATE [Answer].[Student_Answer]
+		SET [Answer_Degree] = 0
+		WHERE [Id] = (SELECT [Answer_Id] FROM deleted);
+	END
+	
+	DECLARE @Answer_Id INT = (SELECT [Answer_Id] FROM inserted);
+	
+	DECLARE @Degree FLOAT;
+	EXEC dbo.CalculateStudentAnswersResults @Student_Answer_Id = @Answer_Id,@Result =@Degree OUTPUT ;
+	UPDATE [Answer].[Student_Answer]
+	SET [Answer_Degree] = @Degree
+	WHERE [Id] = @Answer_Id;
+END
